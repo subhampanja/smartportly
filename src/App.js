8d138d13be9daddefc96574ff17c435a74363b62
subhampanja <subhampanja28@gmail.com>
2021-08-03 23:05:12 +0530

    github fork used

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./App.css";
 import Main from "./components/Main";
+import GitHubForkRibbon from 'react-github-fork-ribbon';
 
 function App() {
   return (
     <div className="app">
+      <GitHubForkRibbon
+        color="green"
+        position="left">
+        New website coming
+      </GitHubForkRibbon>
       <Main />
     </div>
   );
